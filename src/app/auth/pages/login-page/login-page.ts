e2e9f0627e15580028cd2df60e9aa8d41a8a7d62
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-16 13:34:11 +0100

    [WIP] Login form management

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,10 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/authService';
+import { email } from '@angular/forms/signals';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterOutlet],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './login-page-component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPage { }
+export class LoginPage {
+
+  router = inject(Router);
+  fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  authService = inject(AuthService);
+
+  OnSubmit() {
+    const { email = '', password = '' } = this.loginForm.value;
+    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/')
+        console.log('login correcto');
+      }
+    });
+  }
+}
