884230504eee70d06e37da04ad4ccb5250f0852f
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-16 11:40:31 +0100

    Initial routes to Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes'),
+       /* canMatch: [
+            NotAuthenticatedGuard,
+        ] */
+    },
+    {
+    path: '**',
+    redirectTo: 'auth'
+  },
+];
