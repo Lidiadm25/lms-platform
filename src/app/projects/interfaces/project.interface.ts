aeb88a5a6eb39799ba6e7441bc00ad2beba3d08c
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-17 11:28:58 +0100

    New interface to retrieve projects with pagination included and fix in ProjectsService

--- src/app/projects/interfaces/project.interface.ts
+++ src/app/projects/interfaces/project.interface.ts
@@ -1,8 +1,16 @@
+export interface ProjectsResponse {
+  count: number;
+  pages: number;
+  projectsWithStudents: Project[];
+
+}
+
 export interface Project {
     id:          string;
     title:       string;
     description: string;
     duration:    number;
+    studentsCount: number;
 }
 
 
--- src/app/projects/services/ProjectService.ts
+++ src/app/projects/services/ProjectService.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
-import { Project } from '../interfaces/project.interface';
+import { Project, ProjectsResponse } from '../interfaces/project.interface';
 import { RESTProject } from '../interfaces/rest-project.interface';
 import { environment } from '../../../environments/environment';
 import { ProjectMapper } from '../mappers/project.mapper';
 
 const BASE_URL = environment.baseUrl;
-
+interface Options {
+  limit?: number;
+  offset?: number;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -17,14 +20,20 @@ export class ProjectService {
 
   private http = inject(HttpClient)
 
-  getProjects():Observable<Project[]>{
-    return this.http.get<RESTProject[]>(`${BASE_URL}/project`).pipe
+  getProjects(options: Options):Observable<ProjectsResponse>{
+    const  {limit = 10, offset = 0} = options;
+    return this.http.get<ProjectsResponse>(`${BASE_URL}/project`, {
+      params: {
+        limit,
+        offset
+      }
+    }).pipe
     (
-      map(restProjects => ProjectMapper.mapperToProjectArray(restProjects)),
-      catchError(error => {
-        console.log(error);
-        return throwError(() => new Error('Couldnt get any info'))
-      })
+      // map(restProjects => ProjectMapper.mapperToProjectArray(restProjects)),
+      // catchError(error => {
+      //   console.log(error);
+      //   return throwError(() => new Error('Couldnt get any info'))
+      // })
     )
   }
 
