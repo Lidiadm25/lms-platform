85fc90d78e7b9021e1181d7f70b1b0743ece3c60
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-18 09:03:02 +0100

    Guard to verificate auth status

---
+++ src/app/auth/guards/not-authenticated.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { AuthService } from './../services/authService';
+import { Router, type CanMatchFn } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+
+export const notAuthenticatedGuard: CanMatchFn = async (route, segments) => {
+  
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+  const isAuthenticated = await firstValueFrom(authService.checkStatus());
+
+  if(isAuthenticated){
+    router.navigateByUrl('/');
+    return false;
+  }
+  return true;
+};
