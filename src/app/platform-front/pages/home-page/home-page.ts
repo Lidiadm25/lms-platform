af00b5b59c9f88e863811d713e0b848b4ab03404
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-18 14:42:07 +0100

    [WIP] Grid of projects and search bar

--- src/app/platform-front/pages/home-page/home-page.ts
+++ src/app/platform-front/pages/home-page/home-page.ts
@@ -1,9 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ProjectCard } from "../../../projects/components/project-card/project-card";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProjectService } from '../../../projects/services/ProjectService';
+import { PaginationService } from '../../../shared/components/pagination.component/pagination.service';
+import { PaginationComponent } from "../../../shared/components/pagination.component/pagination.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProjectCard, PaginationComponent],
   templateUrl: './home-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage { }
+export class HomePage { 
+
+  projectService = inject(ProjectService);
+  paginationService = inject(PaginationService)
+
+   projectResource = rxResource({
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
+    stream: ({params}) => {
+     return  this.projectService.getProjects({offset: params.page * 9})
+    },
+  });
+
+  onSearch(query:string){
+    console.log("entra en metodo")
+    this.projectResource = rxResource({
+     params: () => ({ page: this.paginationService.currentPage() - 1, query }),
+    stream: ({params}) => {
+     return  this.projectService.getFilteredProjects({offset: params.page * 9}, params.query)
+    },
+  });
+  }
+}
