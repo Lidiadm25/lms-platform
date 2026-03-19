78c07e9182840e5d402c1b519b8665f2d12854c4
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-19 12:12:37 +0100

    Working feature of search bar

--- src/app/platform-front/pages/home-page/home-page.ts
+++ src/app/platform-front/pages/home-page/home-page.ts
@@ -1,35 +1,50 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ProjectCard } from "../../../projects/components/project-card/project-card";
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProjectService } from '../../../projects/services/ProjectService';
 import { PaginationService } from '../../../shared/components/pagination.component/pagination.service';
 import { PaginationComponent } from "../../../shared/components/pagination.component/pagination.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProjectCard, PaginationComponent],
+  imports: [ProjectCard, PaginationComponent, FormsModule],
   templateUrl: './home-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage { 
-
+  router = inject(Router);
+  route = inject(ActivatedRoute)
   projectService = inject(ProjectService);
   paginationService = inject(PaginationService)
-
+  query = '';
+  search = signal('');
    projectResource = rxResource({
-    params: () => ({ page: this.paginationService.currentPage() - 1 }),
+    params: () => ({ page: this.paginationService.currentPage() - 1, query: this.search() }),
     stream: ({params}) => {
-     return  this.projectService.getProjects({offset: params.page * 9})
+      if(params.query.length==0) {
+        return  this.projectService.getProjects({offset: params.page * 9})
+      } else {
+        return this.projectService.getFilteredProjects({offset: params.page * 9, limit: 5}, params.query)
+        
+      }
     },
   });
-
-  onSearch(query:string){
-    console.log("entra en metodo")
-    this.projectResource = rxResource({
-     params: () => ({ page: this.paginationService.currentPage() - 1, query }),
-    stream: ({params}) => {
-     return  this.projectService.getFilteredProjects({offset: params.page * 9}, params.query)
-    },
-  });
+ 
+  onSearch(){
+    console.log("entra")
+   this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { search: this.search() || null }, // null removes the param if empty
+      queryParamsHandling: 'merge', // keep existing params
+      replaceUrl: true // avoid adding to browser history
+    });
   }
+
+  reload() {
+    this.projectResource.reload();
+    this.search.set("a");
+  }
+  
 }
