e1f5321f5993d9954fd6893d47e4ecfa9fd53c9c
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-23 10:54:59 +0100

    [WIP] New page to see the details of a project as User

--- src/app/platform-front/pages/project-details/project-details.ts
+++ src/app/platform-front/pages/project-details/project-details.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Project } from '../../../projects/interfaces/project.interface';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../../../projects/services/ProjectService';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-project-details',
@@ -8,4 +13,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class ProjectDetails {
 
-}
+  route = inject(ActivatedRoute);
+  projectsService = inject(ProjectService);
+  idProject = toSignal(this.route.params.pipe(map(({ idProject })=> idProject)))
+  
+    projectResource = rxResource({
+      params: () =>  ({
+      id: this.idProject(),
+    }),
+  
+    stream: ({params }) => {
+      return this.projectsService.getById(params.id)
+  }})
+
+  
+} 
