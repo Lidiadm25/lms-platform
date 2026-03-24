cdea766d683ed75cd4f526589c62278dd3247ebe
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-24 13:32:13 +0100

    Verification that the image of the project is present, if not placeholder

--- src/app/projects/components/project-card/project-card.ts
+++ src/app/projects/components/project-card/project-card.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../../auth/services/authService';
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { Project } from '../../interfaces/project.interface';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 import { UsersProjectService } from '../../services/UsersProjectService';
 import { UserProject, UserProjectCreate } from '../../../auth/interfaces/user.interface';
@@ -14,30 +14,31 @@ import { User } from '../../interfaces/rest-project.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectCard {
-    project = input.required<Project>();
-    image = "https://as2.ftcdn.net/jpg/05/97/47/95/1000_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg"
-    userProjectService = inject(UsersProjectService);
-    authService = inject(AuthService); 
-    user!:User ;
+  project = input.required<Project>();
 
-
-
+  ngOnInit():void {
   
+    if(this.project().image.length<=0){
+      this.project().image = 'https://as2.ftcdn.net/jpg/05/97/47/95/1000_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg';
+      
+    }
+  }
+  userProjectService = inject(UsersProjectService);
+  authService = inject(AuthService);
+  user!: User;
 
-    inscription(id:string){
-       if(this.authService.user() !== null){
-        this.user = this.authService.user() as User ;
-       }
-    const userProjectLike:UserProjectCreate = {
+  inscription(id: string) {
+    if (this.authService.user() !== null) {
+      this.user = this.authService.user() as User;
+    }
+    const userProjectLike: UserProjectCreate = {
       userId: this.user.id,
       projectId: id,
-    }
-     console.log(userProjectLike)
-        this.userProjectService.addUser(userProjectLike).subscribe({
-          next: res => console.log(res),
-          error: res => console.log(res)
-        });
-      
-      
-    }
- }
+    };
+
+    this.userProjectService.addUser(userProjectLike).subscribe({
+      next: (res) => console.log(res),
+      error: (res) => console.log(res),
+    });
+  }
+}
