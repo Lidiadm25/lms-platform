7aec514e752b6c8c8346b5b3f54cbef26bf621f6
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-25 14:25:16 +0100

    Get all users in user service

---
+++ src/app/auth/services/userService.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+const baseUrl = environment.baseUrl;
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+  private http = inject(HttpClient);
+
+  getUsers():Observable<User[]>{
+    return this.http.get<User[]>(`${baseUrl}/auth/`);
+  }
+}
