a2e7068e74ba104434b07f4b936a87b3b2c1949a
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-07 10:38:21 +0200

    [WIP]  working inscription and retrieve of data from search tags component

--- src/app/admin-dashboard/pages/project-manager-page/project-manager-page.ts
+++ src/app/admin-dashboard/pages/project-manager-page/project-manager-page.ts
@@ -1,11 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  effect,
-  inject,
-  model,
-  signal,
-} from '@angular/core';
+import { UserProjectCreate } from './../../../auth/interfaces/user.interface';
+import { ChangeDetectionStrategy, Component, effect, inject, model, signal } from '@angular/core';
 import { ProjectService } from '../../../projects/services/ProjectService';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -13,9 +7,11 @@ import { rxResource } from '@angular/core/rxjs-interop';
 import { firstValueFrom } from 'rxjs';
 
 import { ImageInput } from '../../components/image-input/image-input';
-import { SearchTags } from "../../components/search-tags/search-tags";
-import { UnitsList } from "../../components/units-list/units-list";
+import { SearchTags } from '../../components/search-tags/search-tags';
+import { UnitsList } from '../../components/units-list/units-list';
 import { Project } from '../../../projects/interfaces/project.interface';
+import { Location } from '@angular/common';
+import { UsersProjectService } from '../../../projects/services/UsersProjectService';
 
 @Component({
   selector: 'app-project-manager-page',
@@ -26,36 +22,36 @@ import { Project } from '../../../projects/interfaces/project.interface';
 export class ProjectManagerPage {
   // Services
   projectService = inject(ProjectService);
+  userProjectService = inject(UsersProjectService);
   activatedRoute = inject(ActivatedRoute);
   // Form
   fb = inject(FormBuilder);
 
   // Variables
   projectId: string = this.activatedRoute.snapshot.params['idProject'];
-  file: File| undefined = undefined;
+  file: File | undefined = undefined;
 
   projectResource = rxResource({
     params: () => ({ id: this.projectId }),
     stream: ({ params }) => this.projectService.getById(params.id),
   });
 
-  project:Project | undefined;
+  project: Project | undefined;
 
   imageUrl = model<string>('');
 
-  constructor() {
-    
+  constructor(private location: Location) {
     effect(() => {
-      this.project=this.projectResource.value();
-      
-      if (this.project?.image) {
-        console.log("aqui3")
-        this.imageUrl.set(`http://localhost:3000/api/files/project/${this.project!.image}`);
+      this.project = this.projectResource.value();
+
+      if (this.project) {
         this.projectForm.patchValue(this.project);
-        console.log("en project manager" + this.imageUrl())
+        if (this.project.image)
+          this.imageUrl.set(`http://localhost:3000/api/files/project/${this.project!.image}`);
       } else {
-        console.log("aqui2")
-        this.imageUrl.set('https://as2.ftcdn.net/jpg/05/97/47/95/1000_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg')
+        this.imageUrl.set(
+          'https://as2.ftcdn.net/jpg/05/97/47/95/1000_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg',
+        );
       }
     });
   }
@@ -73,10 +69,25 @@ export class ProjectManagerPage {
       ...(this.projectForm.value as any),
     };
 
-    await firstValueFrom(this.projectService.updateProject(this.projectId, projectLike, this.file ));
+    await firstValueFrom(this.projectService.updateProject(this.projectId, projectLike, this.file));
+
+    if (this.users.length > 0) {
+      let user: UserProjectCreate = {
+        projectId: this.projectId,
+        userEmail: this.users[0],
+      };
+      await this.userProjectService.addUser(user).subscribe(() => console.log('User added'));
+    }
   }
 
- 
-  // TODO transaction that also collects the manage students changes of project
+  deleteProject() {
+    this.projectService.delete(this.projectId).subscribe(() => console.log('Lesson deleted'));
+    this.location.back();
+  }
 
+  users: Array<string> = [];
+
+  usersInscription(event: any) {
+    this.users = event;
+  }
 }
