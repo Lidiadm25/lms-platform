110a1d05d96c3df336f4f458a0aa24b6d08e47d3
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-08 14:51:35 +0200

    [WIP] fixing reactive forms

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router, RouterLink} from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/authService';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-login-page',
@@ -10,24 +11,37 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPage {
-
   router = inject(Router);
   fb = inject(FormBuilder);
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required]],
   });
-
+  hasError = signal(false);
   authService = inject(AuthService);
+  formUtils = FormUtils;
 
   OnSubmit() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
     const { email = '', password = '' } = this.loginForm.value;
     this.authService.login(email!, password!).subscribe((isAuthenticated) => {
       if (isAuthenticated && this.authService.isAdmin()) {
-        this.router.navigateByUrl('/admin')
-      } else if(isAuthenticated) {
-        this.router.navigateByUrl('/')
+        this.router.navigateByUrl('/admin');
+      } else if (isAuthenticated) {
+        this.router.navigateByUrl('/');
       }
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
     });
   }
 }
--- src/app/auth/pages/register-page/register-page.ts
+++ src/app/auth/pages/register-page/register-page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/authService';
@@ -8,7 +8,7 @@ import { FormErrorLabel } from "../../../shared/components/form-error-label/form
 
 @Component({
   selector: 'app-register-page',
-  imports: [RouterLink, ReactiveFormsModule, FormErrorLabel],
+  imports: [ ReactiveFormsModule],
   templateUrl: './register-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -16,6 +16,8 @@ export class RegisterPage {
   
   router = inject(Router);
   fb = inject(FormBuilder);
+
+  hasError = signal(false)
   registerForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -30,15 +32,25 @@ export class RegisterPage {
   
 
   OnSubmit() {
+    this.registerForm.markAllAsTouched()
     const { email = '', password = '', name= '' } = this.registerForm.value;
-    console.log(email, password, name)
     this.authService.register(email!, password!, name!).subscribe((isAuthenticated) => {
       if (isAuthenticated && this.authService.isAdmin()) {
         this.router.navigateByUrl('/admin')
+        return;
 
       } else if(isAuthenticated) {
         this.router.navigateByUrl('/')
+        return;
       }
+
+        this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
     });
+
+   
+ 
   }
  }
