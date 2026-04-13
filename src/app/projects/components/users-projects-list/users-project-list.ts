ccd00b84574281b2023e174c98da0053019fd6b1
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-13 09:17:37 +0200

    [WIP] checkboxes and selectAll checkbox

--- src/app/projects/components/users-projects-list/users-project-list.ts
+++ src/app/projects/components/users-projects-list/users-project-list.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { UsersProjectResponse } from '../../../auth/interfaces/user.interface';
 
 @Component({
@@ -7,9 +7,24 @@ import { UsersProjectResponse } from '../../../auth/interfaces/user.interface';
   templateUrl: './users-project-list.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersProjectList { 
-
+export class UsersProjectList {
   users = input.required<UsersProjectResponse>();
+  selectedAll = signal(false);
 
-  
+  toggleAll(event: any) {
+   
+ 
+    let checkboxes = document.querySelectorAll('input');
+    event.target.checked ? this.selectedAll.set(true) : this.selectedAll.set(false)
+   
+    checkboxes.forEach((element) => {
+      if (element.name == 'cb' && this.selectedAll() == true) {
+      
+        element.checked = true;
+      } else if (element.name == 'cb' && this.selectedAll() == false) {
+        element.checked = false;
+      }
+    });
+
+  }
 }
