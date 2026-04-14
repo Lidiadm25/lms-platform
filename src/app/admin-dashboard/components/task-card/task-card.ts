8ea2fb226f6045860872f7a7982a3fad12be21d5
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-14 13:26:23 +0200

    Fix navigation between components

--- src/app/admin-dashboard/components/task-card/task-card.ts
+++ src/app/admin-dashboard/components/task-card/task-card.ts
@@ -1,26 +1,24 @@
 import { ChangeDetectionStrategy, Component, inject, input, signal } from '@angular/core';
 import { Task } from '../../../projects/interfaces/project.interface';
 import { TaskService } from '../../../projects/services/TaskService';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './task-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskCard {
-
-
-  taskLoaded = signal<Task|null>(null);
-  activatedRoute = inject(ActivatedRoute)
-  taskService= inject(TaskService)
+  taskLoaded = signal<Task | null>(null);
+  activatedRoute = inject(ActivatedRoute);
+  taskService = inject(TaskService);
   taskId = input.required<string>();
+  lessonId = input.required<string>();
 
   ngOnInit() {
-   
-    this.taskService.getById(this.taskId()).subscribe((result)=> {
+    this.taskService.getById(this.taskId()).subscribe((result) => {
       this.taskLoaded.set(result);
-    })
+    });
   }
- }
+}
--- src/app/admin-dashboard/components/units-list/units-list.ts
+++ src/app/admin-dashboard/components/units-list/units-list.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { UnitCard } from "./unit-card/unit-card";
+import { UnitCard } from './unit-card/unit-card';
 import { Unit } from '../../../projects/interfaces/project.interface';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-units-list',
-  imports: [UnitCard, RouterLink],
+  imports: [UnitCard],
   templateUrl: './units-list.html',
 
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,10 +14,9 @@ export class UnitsList {
   units = input.required<Unit[]>();
   projectId = input.required<string>();
   router = inject(Router);
-  route = inject(ActivatedRoute)
+  route = inject(ActivatedRoute);
 
-  onDoubleClick(id:string){
-    this.router.navigate(['admin/units-manager/', this.projectId() , id ])
+  onDoubleClick(id: string) {
+    this.router.navigate(['admin/units-manager/', this.projectId(), id]);
   }
-  }
- 
+}
