96a2433fdb98b7aeb0128bb0e5fa4d7055658fcb
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-15 13:39:05 +0200

    FIX: calendar updates once change detection is default

--- src/app/platform-front/pages/home-page/home-page.ts
+++ src/app/platform-front/pages/home-page/home-page.ts
@@ -1,34 +1,36 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { CoursesSlider } from '../../components/courses-slider/courses-slider';
+import { Component, inject, signal } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, EventInput } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { TaskService } from '../../../projects/services/TaskService';
 import { DateEventMapper } from '../../../projects/mappers/dateEvent.mapper';
+import { TaskService } from '../../../projects/services/TaskService';
+import { CoursesSlider } from "../../components/courses-slider/courses-slider";
 
 @Component({
   selector: 'app-home-page',
   imports: [CoursesSlider, FullCalendarModule],
   templateUrl: './home-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
+export class HomePage { 
+  
+
+
   events = signal<EventInput[]>([]);
 
-  calendarOptions: CalendarOptions = {
+   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin],
     height: 650,
-    events: [],
+    events: []
+    
   };
-
-  taskService = inject(TaskService);
-
+  taskService = inject(TaskService)
+   
   ngOnInit() {
     // recoger tareas del user
-    this.taskService.getSubmissions()?.subscribe((result) => {
-      this.events.set(DateEventMapper.mapSubmitToEvent(result));
-      this.calendarOptions.events = this.events();
-    });
+    this.taskService.getSubmissions()?.subscribe((result)=> {
+      this.events.set(DateEventMapper.mapSubmitToEvent(result))
+      this.calendarOptions.events = this.events()
+    })
   }
 }
