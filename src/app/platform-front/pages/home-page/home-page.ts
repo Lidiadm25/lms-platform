1a0eec5a3aceab67daff1e7cbbcdd8c4e7713270
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-16 12:43:19 +0200

    Calendar handles clickEvent, which opens a modal with specific data from the task

--- src/app/platform-front/pages/home-page/home-page.ts
+++ src/app/platform-front/pages/home-page/home-page.ts
@@ -1,37 +1,53 @@
 import { Component, inject, signal } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions, EventInput } from '@fullcalendar/core'; // useful for typechecking
+import { CalendarOptions, EventClickArg, EventInput } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { DateEventMapper } from '../../../projects/mappers/dateEvent.mapper';
 import { TaskService } from '../../../projects/services/TaskService';
-import { CoursesSlider } from "../../components/courses-slider/courses-slider";
+import { CoursesSlider } from '../../components/courses-slider/courses-slider';
+import {
+  MatDialog,
+} from '@angular/material/dialog';
+import { DialogEvents } from '../../../shared/components/dialogEvents/dialogEvents';
 
 @Component({
   selector: 'app-home-page',
   imports: [CoursesSlider, FullCalendarModule],
   templateUrl: './home-page.html',
 })
-export class HomePage { 
-  
-
+export class HomePage {
+  dialog = inject(MatDialog);
 
   events = signal<EventInput[]>([]);
 
-   calendarOptions: CalendarOptions = {
+  calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin],
     height: 650,
     events: [],
-    
-    
+    eventClick: this.handleEventClick.bind(this),
   };
-  taskService = inject(TaskService)
-   // todo toggle calendar visible
+  taskService = inject(TaskService);
+  // todo toggle calendar visible
   ngOnInit() {
     // recoger tareas del user
-    this.taskService.getSubmissions()?.subscribe((result)=> {
-      this.events.set(DateEventMapper.mapSubmitToEvent(result))
-      this.calendarOptions.events = this.events()
+    this.taskService.getSubmissions()?.subscribe((result) => {
+      this.events.set(DateEventMapper.mapSubmitToEvent(result));
+      this.calendarOptions.events = this.events();
+    });
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    
+    this.dialog.open(DialogEvents, {
+      data: {
+        id: clickInfo.event.id,
+        title: clickInfo.event.title,
+        start: clickInfo.event.start,
+        end: clickInfo.event.end,
+        lesson_id: clickInfo.event.extendedProps['lesson_task']
+        
+      }
     })
   }
 }
--- src/app/projects/mappers/dateEvent.mapper.ts
+++ src/app/projects/mappers/dateEvent.mapper.ts
@@ -1,17 +1,17 @@
 import { EventInput } from "@fullcalendar/core/index.js";
-import { Submit, SubmitTaskResponse } from "../../admin-dashboard/interfaces/task.interface";
-import { Task } from "../interfaces/project.interface";
+import { SubmitTaskResponse } from "../interfaces/tasks.interface.ts";
 const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
 export class DateEventMapper{
 
     static mapSubmitToEvent(submitItem: SubmitTaskResponse): EventInput[]{
         let events : EventInput[] = []
-       for (let index = 0; index < submitItem.tasks.length; index++) {
+       for (let index = 0; index < submitItem.tasks.length; index++) {      
             events.push({
                 id: submitItem.tasks[index].id,
                 title: submitItem.tasks[index].task.title, 
                 start:new Date( submitItem.tasks[index].task.task_open),
-                end: new Date( submitItem.tasks[index].task.task_close)
+                end: new Date( submitItem.tasks[index].task.task_close),
+                lesson_task: submitItem.tasks[index].task.lesson_task.id,
 
             })
         
