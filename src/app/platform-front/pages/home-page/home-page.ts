846827325135bd150300d75d5011166f9bfd4309
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-17 08:23:16 +0200

    FIX: Calendar didnt reload events after navigating back and forth

--- src/app/platform-front/pages/home-page/home-page.ts
+++ src/app/platform-front/pages/home-page/home-page.ts
@@ -1,14 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, EventClickArg, EventInput } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { DateEventMapper } from '../../../projects/mappers/dateEvent.mapper';
 import { TaskService } from '../../../projects/services/TaskService';
+import { DialogEvents } from '../../../shared/components/dialog-events/dialogEvents';
 import { CoursesSlider } from '../../components/courses-slider/courses-slider';
-import {
-  MatDialog,
-} from '@angular/material/dialog';
-import { DialogEvents } from '../../../shared/components/dialogEvents/dialogEvents';
 
 @Component({
   selector: 'app-home-page',
@@ -27,27 +25,31 @@ export class HomePage {
     events: [],
     eventClick: this.handleEventClick.bind(this),
   };
+
   taskService = inject(TaskService);
   // todo toggle calendar visible
+  constructor(private cd: ChangeDetectorRef) {}
   ngOnInit() {
     // recoger tareas del user
     this.taskService.getSubmissions()?.subscribe((result) => {
       this.events.set(DateEventMapper.mapSubmitToEvent(result));
+
       this.calendarOptions.events = this.events();
+      this.cd.detectChanges();
     });
   }
+  // todo  copy of the object, make your change, and then reassign it.
 
   handleEventClick(clickInfo: EventClickArg) {
-    
     this.dialog.open(DialogEvents, {
       data: {
         id: clickInfo.event.id,
         title: clickInfo.event.title,
         start: clickInfo.event.start,
         end: clickInfo.event.end,
-        lesson_id: clickInfo.event.extendedProps['lesson_task']
-        
-      }
-    })
+        lesson_id: clickInfo.event.extendedProps['lesson_task'],
+        task_id: clickInfo.event.extendedProps['task_id'],
+      },
+    });
   }
 }
