600f8f19dd09eeff7fb312b61b42d49b6baeb0fd
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-22 11:53:05 +0200

    grade review form, it dinamically gets disabled/enabled depending if the submission from the specific user exists

--- src/app/admin-dashboard/components/grade-table/grade-table.ts
+++ src/app/admin-dashboard/components/grade-table/grade-table.ts
@@ -1,13 +1,37 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Submit } from '../../../projects/interfaces/tasks.interface.ts';
 
 @Component({
   selector: 'app-grade-table',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './grade-table.html',
-  styleUrl: './grade-table.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GradeTable {
   userId = input.required<string>();
-  submit = input.required<string>();
- }
+  submit = input.required<Submit | null>();
+  fb = inject(FormBuilder);
+
+  submitForm = this.fb.group({
+    min_range: '',
+    max_range: '',
+    feedback: '',
+    total: '',
+  });
+
+  constructor() {
+    effect(() => {
+      const disabled = !this.submit();
+
+      Object.values(this.submitForm.controls).forEach((control) => {
+        if (disabled) {
+          control.disable({ emitEvent: false });
+        } else {
+          control.enable({ emitEvent: false });
+        }
+      });
+    });
+  }
+
+  onSubmit() {}
+}
