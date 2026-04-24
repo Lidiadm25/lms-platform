3549237144438173c58ace99eade147d753af9eb
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-24 13:33:21 +0200

    refactor: new order for manager pages

--- src/app/admin-dashboard/admin-front.routes.ts
+++ src/app/admin-dashboard/admin-front.routes.ts
@@ -10,55 +10,59 @@ import { UsersProjectPage } from './pages/users-project-page/users-project-page'
 import { UnitsManagerPage } from './pages/units-manager-page/units-manager-page';
 import { TaskManagerPage } from './pages/task-manager-page/task-manager-page';
 import { GradesManagerPage } from './pages/grades-manager-page/grades-manager-page';
-
+import { ManagerPage } from './pages/manager-page/manager-page';
 
 export const platformFrontRoutes: Routes = [
-    {
+  {
+    path: '',
+    component: DashboardLayout,
+    canMatch: [isAdminGuard],
+    children: [
+      {
         path: '',
-        component: DashboardLayout,
-        canMatch:[isAdminGuard],
-        children:[
-            {
-            path: '',
-            component: DashboardPage
-            }
-            ,
-            {
-             path:'admin-project',
-             component: ProjectManagerPage
-            },
-            {
-             path:'projects',
-             component: ProjectsPage
-            },
-             {
-             path:'users-project/:idProject',
-             component: UsersProjectPage
-            },
-            {
-             path:'project-manager/:idProject',
-             component: ProjectManagerPage
-            },
-            {
-            path:'lesson-manager/:idUnit/:idLesson',
-            component: LessonManagerPage
-            },
-            {
-                path:'units-manager/:idProject/:idUnit',
-                component: UnitsManagerPage
-            },
-            {
-                path:'tasks-manager/:idLesson/:idTask',
-                component: TaskManagerPage
-            },
-            {
-                path:'grades-manager/:idProject',
-                component: GradesManagerPage
-            }
+        component: DashboardPage,
+      },
+      {
+        path: 'admin-project',
+        component: ProjectManagerPage,
+      },
+      {
+        path: 'projects',
+        component: ProjectsPage,
+      },
+      {
+        path: 'users-project/:idProject',
+        component: UsersProjectPage,
+      },
 
-        ]
-    },
-    
+      {
+        path: 'grades-manager/:idProject',
+        component: GradesManagerPage,
+      },
+      {
+        path: 'manager/:idProject',
+        component: ManagerPage,
+        children: [
+          {
+            path: '',
+            component: ProjectManagerPage,
+          },
+          {
+            path: ':idUnit',
+            component: UnitsManagerPage,
+          },
+          {
+            path: ':idUnit/:idLesson',
+            component: LessonManagerPage,
+          },
+          {
+            path: ':idUnit/:idLesson/:idTask',
+            component: TaskManagerPage,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default platformFrontRoutes;
