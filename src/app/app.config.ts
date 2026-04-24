16bdf88336e054f5aa17f88e24892f67c867efff
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-24 10:52:29 +0200

    feat: install new library ngx-quill test: using quill component in project manager

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideQuillConfig } from 'ngx-quill';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -16,5 +17,28 @@ export const appConfig: ApplicationConfig = {
         authInterceptor,
       ])
     ),
+    provideQuillConfig({
+      modules: {
+        toolbar: [
+         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['code-block'],
+  ['link', 'image', 'video'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+
+        ]
+      }
+    })
   ]
 };
