9aa17a187b9e7a3ce0defc0e0a677f4443f33bc7
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-28 13:21:51 +0200

    styles: small changes of styling and format code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,30 +15,28 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         // loggingInterceptor,
         authInterceptor,
-      ])
+      ]),
     ),
     provideQuillConfig({
       modules: {
         toolbar: [
-         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['code-block'],
-  ['link', 'image', 'video'],
+          ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+          ['code-block'],
+          ['link', 'image', 'video'],
 
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+          [{ header: 1 }, { header: 2 }], // custom button values
+          [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+          [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
 
+          [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-
-
-        ]
-      }
-    })
-  ]
+          [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+          [{ font: [] }],
+          [{ align: [] }],
+        ],
+      },
+     
+    }),
+  ],
 };
