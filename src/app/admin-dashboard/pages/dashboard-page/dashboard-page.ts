074ec64a2119312bdf76c8daf0091b480aa17623
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-04 14:18:20 +0200

    feat: install of chartjs, trying if example works

--- src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
+++ src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChartDashboard } from "../../components/chart-dashboard/chart-dashboard";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [ChartDashboard],
   templateUrl: './dashboard-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardPage { }
+export class DashboardPage {
+}
