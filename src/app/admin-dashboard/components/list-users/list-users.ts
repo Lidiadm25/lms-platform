95e0edbb0503b643b2a590e159d6bc82050b139f
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-06 14:51:08 +0200

    feat: now user stays selected when clicked

--- src/app/admin-dashboard/components/list-users/list-users.ts
+++ src/app/admin-dashboard/components/list-users/list-users.ts
@@ -17,15 +17,35 @@ export class ListUsers {
   page = this.paginationService.currentPage() - 1;
   usersList = signal<UsersProjectResponse | null>(null);
   userChosen = output<string>()
-  
+  selected = signal<boolean>(true)
   ngOnInit(){
     this.usersProjectService.getUsers(this.projectId(),{offset: this.page * 9}).subscribe((result) => {this.usersList.set(result)
       console.log(result)
     } )
   }
 
+ onButtonGroupClick(event: Event){
+
+  console.log("entra")
+
+    let clickedElement = event.target as HTMLButtonElement
+
+
+
+      let isCertainButtonAlreadyActive = clickedElement!.parentElement!.querySelector(".active");
+      console.log("Valor de activo: " + isCertainButtonAlreadyActive)
+      // if a Button already has Class: .active
+      if( isCertainButtonAlreadyActive ) {
+        isCertainButtonAlreadyActive.classList.remove("active");
+      }
+
+      clickedElement!.className += " active";
+    
+
+  }
+
   emitUser(id:string){
-   
+    this.selected.set(true)
     this.userChosen.emit(id);
   }
 
