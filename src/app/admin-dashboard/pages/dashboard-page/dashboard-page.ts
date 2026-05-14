dcd50d83a1270b5d3192b8653154e51d83a06704
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-14 10:16:46 +0200

    feat: now dashboard collects all online users, still wip filter of the users that are students

--- src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
+++ src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ChartDashboard } from "../../components/chart-dashboard/chart-dashboard";
+import { SocketService } from '../../../auth/services/socketService';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -8,4 +9,10 @@ import { ChartDashboard } from "../../components/chart-dashboard/chart-dashboard
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardPage {
+
+  socketService = inject(SocketService)
+  totalOnline = signal<number>(0)
+  constructor(){
+    this.socketService.getConnectedClients().subscribe((data)=> this.totalOnline.set(data.length))
+  }
 }
