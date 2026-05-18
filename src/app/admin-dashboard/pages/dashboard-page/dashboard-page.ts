52b699b39ae229b0b988f33d1b5e1ab5cf8dd386
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 11:27:20 +0200

    feat: dashboard requests to load general data

--- src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
+++ src/app/admin-dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { ChartDashboard } from "../../components/chart-dashboard/chart-dashboard";
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChartDashboard } from '../../components/chart-dashboard/chart-dashboard';
 import { SocketService } from '../../../auth/services/socketService';
+import { UsersProjectService } from '../../../projects/services/UsersProjectService';
+import { OnlineClient } from '../../../shared/components/my-drawer/chat-component/interfaces/online.interface';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -9,10 +12,29 @@ import { SocketService } from '../../../auth/services/socketService';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardPage {
+  socketService = inject(SocketService);
+  totalOnlineClients = signal<OnlineClient[]>([]);
+  totalAllTime = signal<number>(0);
+  totalCurrent = signal<number>(0);
+  usersCurrent = signal<Partial<User[]>>([]);
+  userProjectService = inject(UsersProjectService);
 
-  socketService = inject(SocketService)
-  totalOnline = signal<number>(0)
-  constructor(){
-    this.socketService.getConnectedClients().subscribe((data)=> this.totalOnline.set(data.length))
+  onlineFiltered = computed(() => {
+    const totalOnline = this.totalOnlineClients();
+    var newCounter = 0;
+    for (let index = 0; index < totalOnline.length; index++) {
+      const element = totalOnline[index];
+      if (element.user_id.match(this.usersCurrent()[index]!.id)) {
+        newCounter++;
+      }
+    }
+
+    return newCounter;
+  });
+
+  constructor() {
+    this.socketService.getConnectedClients().subscribe((data) => this.totalOnlineClients.set(data));
+    this.userProjectService.getAllTime().subscribe((data) => this.totalAllTime.set(data));
+    this.userProjectService.getCurrent().subscribe((data) => this.usersCurrent.set(data));
   }
 }
