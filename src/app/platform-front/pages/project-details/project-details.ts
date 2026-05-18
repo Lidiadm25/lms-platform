352109c1ecad4d2673a5174503401fc2f7baf2c0
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 11:37:17 +0200

    feat: get avg rating of a course request and fill starts depending on its value

--- src/app/platform-front/pages/project-details/project-details.ts
+++ src/app/platform-front/pages/project-details/project-details.ts
@@ -9,10 +9,13 @@ import { UsersProjectService } from '../../../projects/services/UsersProjectServ
 import { UserProjectCreate } from '../../../auth/interfaces/user.interface';
 import { jwtDecode } from 'jwt-decode';
 import { jwtToken } from '../../../auth/interfaces/auth-response.interface';
+import { SurveyUserService } from '../../../projects/services/SurveyUserService';
+import { SafeHTMLPipe } from "../../../pipes/safeHTML-pipe";
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-details',
-  imports: [UnitsLessonsAccordeon],
+  imports: [UnitsLessonsAccordeon, SafeHTMLPipe, DecimalPipe],
   templateUrl: './project-details.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -25,6 +28,8 @@ export class ProjectDetails {
   project = signal<Project | null>(null)
    lastModified!:string;
   usersNumber = signal<number>(0)
+  surveyUserService= inject(SurveyUserService)
+  rating = signal<number>(0)
 
   ngOnInit(){
     this.projectsService.getById(this.idProject()).subscribe({
@@ -33,6 +38,16 @@ export class ProjectDetails {
         this.lastModified = this.project()!.last_modified.toString()
       }
     })
+   
+    this.surveyUserService.getSpecificAvg(this.idProject()).subscribe((data)=>{ 
+      this.rating.set(data)
+      const redondeado = Math.round(data * 2) / 2;
+      var star= document.getElementById(redondeado.toString()) as HTMLDivElement
+      console.log()
+      star.ariaCurrent="true";
+    } )
+   
+    
 
     this.usersProject.getUsers(this.idProject(), null).subscribe((data)=> this.usersNumber.set(data.count))
   }
