b294f62adf1f292a3b706144042ba67a1a6b1d43
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 11:26:33 +0200

    feat: now you can't grade while the task is still open

--- src/app/admin-dashboard/components/grade-table/grade-table.ts
+++ src/app/admin-dashboard/components/grade-table/grade-table.ts
@@ -4,6 +4,7 @@ import { Grade } from '../../../projects/interfaces/grade.interface';
 import { Submit } from '../../../projects/interfaces/tasks.interface';
 import { GradeService } from '../../../projects/services/GradeService';
 import { catchError, of } from 'rxjs';
+import { Task } from '../../../projects/interfaces/project.interface';
 
 @Component({
   selector: 'app-grade-table',
@@ -22,15 +23,27 @@ export class GradeTable {
     total: 0,
   });
 
+  messageError = signal<string>('There was an error');
+  task = input<Task | null>(null);
   wasSaved = signal<boolean>(false);
   hasError = signal<boolean>(false);
   edit = signal<boolean>(false);
-  grade = signal<Grade| null>(null);
+  grade = signal<Grade | null>(null);
 
   constructor() {
     effect(() => {
-      const disabled = !this.submit();
-
+      var disabled = !this.submit();
+       let close = new Date(this.task()!.task_close)
+      if (close > new Date() && this.submit()!.date_send == null) {
+        disabled = true;
+        this.messageError.set('The task is still open');
+        this.hasError.set(true);
+        setTimeout(() => {
+          this.hasError.set(false);
+        }, 3000);
+      } else {
+        disabled = false;
+      }
       Object.values(this.submitForm.controls).forEach((control) => {
         if (disabled) {
           control.disable({ emitEvent: false });
@@ -40,16 +53,13 @@ export class GradeTable {
       });
 
       if (!disabled) {
-        this.gradeService
-          .getGradeFromSubmit(this.submit()!.id)
-          .subscribe((result) => {
-            this.grade.set(result)
-            this.edit.set(true);
-            this.submitForm.patchValue(result)});
+        this.gradeService.getGradeFromSubmit(this.submit()!.id).subscribe((result) => {
+          this.grade.set(result);
+          this.edit.set(true);
+          this.submitForm.patchValue(result);
+        });
       }
     });
-
-
   }
 
   onSubmit() {
@@ -60,30 +70,25 @@ export class GradeTable {
 
     var resultados;
 
-    this.edit() ? resultados=  this.gradeService.updateGrade(gradeLike, this.grade()!.id) : resultados= this.gradeService.create(gradeLike);
+    this.edit()
+      ? (resultados = this.gradeService.updateGrade(gradeLike, this.grade()!.id))
+      : (resultados = this.gradeService.create(gradeLike));
 
-    resultados.pipe(
-     catchError((error: any) => this.launchError())
-
-    ).subscribe((result)=>
-    {
-       this.wasSaved.set(true);
-    setTimeout(() => {
-      this.wasSaved.set(false);
-    }, 3000);
-    }
-    )
-
-
-    
+    resultados.pipe(catchError((error: any) => this.launchError())).subscribe((result) => {
+      this.wasSaved.set(true);
+      setTimeout(() => {
+        this.wasSaved.set(false);
+      }, 3000);
+    });
   }
 
-  launchError(){
+  launchError() {
+    this.messageError.set("There was an error.")
     this.hasError.set(true);
     setTimeout(() => {
       this.hasError.set(false);
     }, 3000);
 
-      return of(false);
+    return of(false);
   }
 }
